2a718f0a18f8304dee6e36019147bc482d32cf13
zaimazhar97 <54124001+zaimazhar97@users.noreply.github.com>
2021-09-01 22:47:44 +0800

    added jwt middleware
    Signed-off-by: zaimazhar97 <54124001+zaimazhar97@users.noreply.github.com>

--- components/jwt.component.js
+++ components/jwt.component.js
@@ -7,14 +7,13 @@ passport.authenticate('jwt', {
 
 function generateJwt(payload) {
   return jsonwebtoken.sign(payload, process.env.PIKP_SECRET)
-  jsonwebtoken.verify()
 }
 
-// function middleware(token) {
-//   retujsonwebtoken.verify(token, process.env.PIKP_SECRET)) {
-//     return true
-//   }
-// }
+function middleware(token) {
+  if(jsonwebtoken.verify(token, process.env.PIKP_SECRET)) {
+    return true
+  }
+}
 
 module.exports = {
   generateJwt,
